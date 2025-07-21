3c2ef2c412270da379b885d97d5cd7597bc6a520
Muralisanku <sankumurali4a6@gmail.com>
2025-07-21 11:27:33 +0530

    terraform-aws

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ module "catalogue" {
   vpc_id = data.aws_ssm_parameter.vpc_id.value
   component_sg_id = data.aws_ssm_parameter.user_sg_id.value
   private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
-  iam_instance_profile = "ShellscriptRoleForRoboshop"
+  iam_instance_profile = var.iam_instance_profile
   project_name = var.project_name
   environment = var.environment
   common_tags = var.common_tags
@@ -12,4 +12,5 @@ module "catalogue" {
   app_alb_listener_arn = data.aws_ssm_parameter.app_alb_listener_arn.value
   rule_priority = 20
   app_version = var.app_version
+  
 }
